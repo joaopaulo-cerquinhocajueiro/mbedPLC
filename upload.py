4bf2fd38261209ca7f78663b1cff1e98ba649232
João Paulo Cerquinho Cajueiro <joaopaulo.cerquinhocajueiro@gmail.com>
2017-06-05 17:34:24 -0300

    Alterações no upload.py - modificador e mais comentários

--- upload.py
+++ upload.py
@@ -1,49 +1,79 @@
-import serial
-import time
+# -*- coding: UTF-8 -*-
+
+import serial # para comunicar com o kl25z
+import time # para separa o envio dos blocos; talvez não seja necessário
+
+# Inicializa a comunicação serial
 porta = 'COM5'
 ser = serial.Serial(porta,9600)
+
+###
+### Parsing
+###
 programa = open('src/programa.txt')
-blocos = []
+blocos = [] # Cada bloco vai ser um comando enviado ao CLP
 for line in programa:
 	pedacos = line.split()
-	if pedacos[0].endswith(':'):
-		pedacos[0] = pedacos[0].strip(':')
+
+	## Rótulo
+	if pedacos[0].endswith(':'): # Se o primeiro pedaço termina em ':' é um rótulo
+		pedacos[0] = pedacos[0].strip(':') # Salva sem o ':'
 	else:
-		pedacos.insert(0,16*' ')
-	pedacos[0] = pedacos[0].ljust(16,' ')
-	pedacos.insert(2,' ')
-	if pedacos[1].endswith('N'):
-		pedacos[2]='N '
-		pedacos[1] = pedacos[1].rstrip('N')
-	if pedacos[1].endswith('C'):
-		pedacos[2] = 'C '
-		pedacos[1] = pedacos[1].rstrip('C')
+		pedacos.insert(0,16*' ') # Se não tiver rótulo, acrescenta um em branco
+	pedacos[0] = pedacos[0].ljust(16,' ') # Ajusta o rótulo para ter 16 caracteres
+
+	## Modificador
+	# Como o modificador vem junto com o comando, é necessário identificá-lo e separá-lo
+	pedacos.insert(2,'') # Insere um pedaço para o modificador
+	# Testa se há operação adiada
 	if pedacos[1].endswith('('):
-		pedacos[2] = '( '
+		pedacos[2] = pedaços[2] + '('
 		pedacos[1] = pedacos[1].rstrip('(')
-	pedacos[1] = pedacos[1].ljust(3,' ')
-	if len(pedacos)==3:
-		pedacos.append('  ')
-	pedacos[3] = pedacos[3].ljust(16,' ')
+	# Testa se há inversão
+	if pedacos[1].endswith('N'):
+		pedacos[2] = pedaços[2] + 'N'
+		pedacos[1] = pedacos[1].rstrip('N')
+	# Testa se há condicional
+	if pedacos[1].endswith('C'):
+		pedacos[2] = pedaços[2] + 'C'
+		pedacos[1] = pedacos[1].rstrip('C')
+	# Ajusta o tamanho do comando e do modificador
+	pedacos[1] = pedacos[1].ljust(4,' ') # comando
+	pedacos[2] = pedacos[2].ljust(2,' ') # modificador
+
+	## Operando
+	if len(pedacos)==3: # Se não houver operando
+		pedacos.append('  ') # Acrescenta um vazio
+	pedacos[3] = pedacos[3].ljust(16,' ') # ajusta  tamanho
+
+	## Salva instrução completa
 	blocos.append(pedacos)
 
-print(blocos)
+#print(blocos)
 
+###
+### Programação
+###
+
+#Envia a quantidade de blocos que o programa tem
 ser.write(bytes(len(blocos)))
 ser.write('\n')
-time.sleep(1)
+# Espera um tempo, por sanidade
+time.sleep(0.1)
 for bloco in blocos:
-	for pedaco in bloco:
-		pedaco = pedaco + '\n'
-		ser.write(pedaco)
-		print(pedaco)
-		time.sleep(0.1)
-	time.sleep(0.2)
-import msvcrt
-while True:
-	if msvcrt.kbhit():
-		break
-	if ser.in_waiting:
+	for pedaco in bloco: # Para cada pedaço de cada bloco
+		pedaco = pedaco + '\n' # Acrescenta uma quebra de linha
+		ser.write(pedaco) # E envia pela serial
+		print(pedaco) # E para o terminal, por simplicidade
+		time.sleep(0.01)
+	time.sleep(0.02)
+
+###
+### Comunicação
+###
+import msvcrt # Para detectar que uma tecla foi apertada
+while not msvcrt.kbhit(): # Enquanto nenhuma tecla for apertada
+	if ser.in_waiting: # Se chegar coisa na serial, imprime
 		s = ser.readline()
 		print(s)
 #s = ser.read(100)
